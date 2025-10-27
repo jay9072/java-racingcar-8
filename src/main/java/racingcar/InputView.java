f9923c155dfb035d0b3a06306d0a33f6126c972d
jay9072 <jay907212@gmail.com>
2025-10-27 20:14:37 +0900

    feat: 사용자 입력 기능

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String[] readCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return input.split(",");
+    }
+
+    public static int readAttemptCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String line = Console.readLine();
+        return Integer.parseInt(line);
+    }
+
+
+}
